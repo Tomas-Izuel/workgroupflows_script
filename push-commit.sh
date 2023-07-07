2a23828191d0f743166863640f6f60cc9be3ebdb
Tomas Izuel <92755383+Tomas-Izuel@users.noreply.github.com>
2023-07-07 11:57:31 -0300

    1.2

--- push-commit.sh
+++ push-commit.sh
@@ -2,14 +2,34 @@ cd "$1"
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 
+read -p "¿Agregar src/* al commit? (y/n): " addSrc
+
+if [ "$addSrc" = "y" ]; then
+    git add src/*
+fi
+
 git commit -m "$2"
 
 git checkout develop
 
 git pull
 
-git checkout $branch
+if [ -n "$(git diff)" ]; then
+    echo "Se han obtenido nuevos cambios."
 
-git merge develop
+    git checkout $branch
 
-git push origin $branch
+    read -p "¿Desea mergear los cambios? (y/n): " mergeValue
+
+    if [ "$mergeValue" = "y" ]; then
+        git merge develop
+    fi
+
+    git push origin $branch
+else
+    echo "No se han obtenido nuevos cambios."
+    
+    git checkout $branch
+
+    git push origin $branch
+fi
